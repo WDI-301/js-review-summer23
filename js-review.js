3fde17949b4d1d45ba15f00dbcec041c06fa1b28
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-05-15 11:01:21 -0700

    rest operator

--- js-review.js
+++ js-review.js
@@ -10,13 +10,26 @@
 // console.log('!@-------callback-------@!')
 // console.log(callBack('Hello', 'World!'))
 
-const callBack3 = (param, param2, param3='World') => {
-    return `${param} ${param2} ${param3}`
+const callBack3 = (param0="Hello", param1="Your", param2='World') => {
+    return `${param0} ${param1} ${param2}`
 }
 
 console.log('!@-------callback-------@!')
 console.log(callBack3('Hello', 'Your'))
+console.log(callBack3('Hello', undefined, 'World!!!!'))
+console.log(callBack3(undefined, undefined, undefined))
 console.log('!@-------callback3-------@!')
 console.log(callBack3('Hello', 'Your', 'World!!!!'))
+
+const restOperator = (...params) => {
+    return params.join(' ')
+}
+
+
+console.log('!@-------2 params-------@!')
+console.log(restOperator("Hello", "World"))
+console.log('!@-------3 params-------@!')
+console.log(restOperator("Hello", "Your", "World"))
+
 
 
